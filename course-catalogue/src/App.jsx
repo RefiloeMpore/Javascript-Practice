58632524d0a189bc84cf470b9845b611853203d6
RefiloeMpore <mporerefiloe@gmail.com>
2026-09-17 23:20:17 +0000

    Exercise 1.2 stage 1 - split into Header, Content, Total

--- course-catalogue/src/App.jsx
+++ course-catalogue/src/App.jsx
@@ -1,19 +1,46 @@
+
+
+function Header({course}) {
+  return <h1>{course}</h1>;
+}
+
+
+
+function Content({part1, part2, part3}){
+return (
+  <div>
+  <p>{part1}</p>
+  <p>{part2}</p>
+  <p>{part3}</p>
+  </div>
+);
+}
+
+function Total({total}) {
+  return  <p>Number of exercises {total}</p>;
+  
+}
+
 function App() {
-  const name = 'Half stack application development';
+  const name = 'Full stack application development';
   const a= 10;
   const b= 7;
-  const c= 14;
+  const c=14; 
 
-  return(
+  return (
     <div>
-      <h1>Half stack application development.</h1>
-      <p>Fundamentals of React 10</p>
-      <p>Using props to pass data 7 </p>
-      <p>State of a component 14</p>
-      <p>Number of exercises {a + b +c}</p> 
-      </div>
+      <Header course={name}/>
+      <Content
+      part1={`Fundamentals of React ${a}`}
+      part2={`Using props to pass data ${b}`}
+      part3={`State of a component ${c}`}
+      />
+      <Total total ={a+b+c}/>
+    </div>
   );
 
+
 }
-  
+
+
 export default App;
